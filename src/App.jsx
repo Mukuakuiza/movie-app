a84cbef6a4e1790f75a85682c54f675c6f8c86ba
Mukuakuiza <hinomukuakuiza12@gmail.com>
2026-02-22 01:12:30 +0200

    created the header with search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import Search from "./components/Search.jsx";
+import {useState} from "react";
 
 function App() {
-
+    const [searchTerm, setSearchTerm] = useState('')
 
   return (
       <main>
@@ -8,8 +10,11 @@ function App() {
 
           <div className="wrapper">
               <header>
+                  <img src="./hero.png" alt="hero"/>
                   <h1>Find <span className="text-gradient">Movies</span> That You Will Enjoy In Here... </h1>
               </header>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+              <h1 className="text-white">{searchTerm}</h1>
           </div>
       </main>
   )
