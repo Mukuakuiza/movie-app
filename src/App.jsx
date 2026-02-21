cd654a543d275042eb3cccf3c5a66d580fc51975
Mukuakuiza <hinomukuakuiza12@gmail.com>
2026-02-22 00:35:40 +0200

    installed tailwind and

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ function App() {
 
 
   return (
-    <>
-    <h1>Welcome</h1>
-    </>
+      <main>
+          <div className="pattern"/>
+
+          <div className="wrapper">
+              <header>
+                  <h1>Find <span className="text-gradient">Movies</span> That You Will Enjoy In Here... </h1>
+              </header>
+          </div>
+      </main>
   )
 }
 
